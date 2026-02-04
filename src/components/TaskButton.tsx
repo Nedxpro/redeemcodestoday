0ef506ba2fd8cf8132018e5bb2c6011782216bbf
Pushpendra Singroul <74776631+Nedxpro@users.noreply.github.com>
2026-02-04 13:40:03 +0530

    Update TaskButton.tsx

--- src/components/TaskButton.tsx
+++ src/components/TaskButton.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import diamondsImage from "@/assets/free fire max.png";
+import diamondsImage from "@/assets/free-fire-max.png";
 interface TaskButtonProps {
   taskLink: string;
 }
